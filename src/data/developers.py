a6319b04c55951a6643ad740293bf54a69089474
Matteo Boschi <mbmaken@gmail.com>
2022-01-19 19:43:27 +0100

    turn on pair-programmin edit developers (leave only io app devs)

--- src/data/developers.py
+++ src/data/developers.py
@@ -3,20 +3,7 @@ Matteo Boschi,,Developers
 Cristiano Tofani,,Developers
 Fabrizio Filizola,,Developers
 Simone Biffi,,Developers
-Giovanni Mancini,,Developers
-Emanuele De Cupis,,Developers
-Pasquale De Vita,,Developers
-Vito Falco,,Developers
-Pasquale Spica,,Developers
-Alessio Dore,,Developers
-Simone Infante,,Developers
-Greta Quadrati,,Developers
-Daniele Manni,,Developers
-Lorenzo Franceschini,,Developers
-Marco Vit,,Developers
-Michael Disarò,,Developers
-Fabrizio Papi,,Developers
-Ivan Diana,,Developers
 Pietro Grandi,,Developers
-Stefano Perazzolo,,Developers
-Jacopo Carlini,,Developers'''
+Diego Pasquali,,Developers
+Emilio Pavia,,Developers
+'''
