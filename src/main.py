f1045dd9e436a36b27abe3c3f6ce5d20c8dff979
Matteo Boschi <mbmaken@gmail.com>
2022-06-14 10:59:33 +0200

    debug

--- src/main.py
+++ src/main.py
@@ -6,22 +6,22 @@ import os
 from utils.github import get_pull_requests_data, GithubStats
 from utils.slack import send_slack_message_blocks
 
-sdfsdf = os.getenv('GH_API_TOKEN')
+github_token = os.getenv('GH_API_TOKEN')
 slack_token = os.getenv('SLACK_TOKEN')
 days_span = os.getenv('DAYS_SPAN', 7)
 slack_channel = os.getenv('SLACK_CHANNEL', 'test_feed')
 github_company_name = os.getenv('GH_COMPANY_NAME', 'heritageholdings')
 github_company_repositories = os.getenv('GH_COMPANY_REPOSITORIES', ['iconic'])
 assert slack_token is not None
-assert sdfsdf is not None
+assert github_token is not None
 
 end = datetime.datetime.now()
 start = end - datetime.timedelta(days=days_span)
 # it assumes that each item is a valid project inside {github_company_name} org
 for github_project in github_company_repositories:
-    repo_stats = " | ".join([f'{v} {k}' for k, v in GithubStats.get_repo_stats(github_project, sdfsdf).items()])
-    pr_created = get_pull_requests_data(sdfsdf, github_project, start, end)
-    pr_reviews = get_pull_requests_data(sdfsdf, github_project, start, end, 'closed', 'merged')
+    repo_stats = " | ".join([f'{v} {k}' for k, v in GithubStats.get_repo_stats(github_project, github_token).items()])
+    pr_created = get_pull_requests_data(github_token, github_project, start, end)
+    pr_reviews = get_pull_requests_data(github_token, github_project, start, end, 'closed', 'merged')
     stats = GithubStats(pr_created, pr_reviews)
     msg = f'These are the contributions included in *<https://github.com/{github_company_name}/{github_project}|{github_project.upper()}>* from *{start.day:02}/{start.month:02}* to *{end.day:02}/{end.month:02}*\n'
     if len(pr_reviews) > 0:
