3be3668e06ce46adda367c426df305f01d9d23ad
Matteo Boschi <mbmaken@gmail.com>
2022-10-26 22:12:54 +0200

    wip

--- src/main.py
+++ src/main.py
@@ -3,20 +3,24 @@
 import datetime
 
 from utils.env import days_span, github_company_repositories, GITHUB_COMPANY_NAME, slack_channel, slack_token
-from utils.github2 import get_repo_stats2, group_by_state
+from utils.github2 import get_repo_stats2, group_by_state, group_by_developer
 from utils.slack import send_slack_message_blocks
 
 end = datetime.datetime.now()
 start = end - datetime.timedelta(days=days_span)
 # it assumes that each item is a valid project inside {GITHUB_COMPANY_NAME} org
-for github_project in github_company_repositories:
-    pull_requests = get_repo_stats2(github_project, days_span)
+for repository in github_company_repositories:
+    pull_requests = get_repo_stats2(repository, days_span)
+    by_state = group_by_state(pull_requests)
     pull_requests_created = list(filter(lambda pr: (end - pr.created_at).days <= days_span, pull_requests))
-    pull_requests_merged = list(filter(lambda pr: pr.merged, pull_requests))
-    pull_requests_reviewed = list(filter(lambda pr: len(pr.reviewers), pull_requests))
-    pull_requests_open_draft_closed = group_by_state(list(filter(lambda pr: pr.state in ["open","draft"], pull_requests)))
-    repo_stats = " | ".join([f'{v} {k}' for k, v in pull_requests_open_draft_closed.items()])
-    msg = f'These are the contributions included in *<https://github.com/{GITHUB_COMPANY_NAME}/{github_project}|{github_project.upper()}>* from *{start.day:02}/{start.month:02}* to *{end.day:02}/{end.month:02}*\n'
+    pull_requests_merged = by_state.get("merged", [])
+    pull_requests_open = by_state.get("open", [])
+    pull_requests_closed = by_state.get("closed", [])
+    pull_requests_reviewed = by_state.get("reviewed", [])
+    pull_requests_open_draft_closed = {"draft": by_state.get("draft", []), "open": by_state.get("open", []),
+                                       "closed": by_state.get("closed", [])}
+    repo_stats = " | ".join([f'{len(v)} {k}' for k, v in pull_requests_open_draft_closed.items()])
+    msg = f'These are the contributions included in *<https://github.com/{GITHUB_COMPANY_NAME}/{repository}|{repository.upper()}>* from *{start.day:02}/{start.month:02}* to *{end.day:02}/{end.month:02}*\n'
     if len(pull_requests_reviewed) > 0:
         for pr in pull_requests_reviewed:
             msg += f'- <{pr.url}|{pr.title.replace("`", "")}>'
@@ -36,6 +40,8 @@ for github_project in github_company_repositories:
     msg += "*Pull requests stats*\n"
     msg += f'created: `{len(pull_requests)}`\n'
     msg += f'reviewed: `{len(pull_requests_reviewed)}`\n'
+    if len(pull_requests_closed):
+        msg += f'closed: `{len(pull_requests_closed)}`\n'
     msg += f'current: `{repo_stats if len(repo_stats) else "all clear!"}`\n'
     thread = send_slack_message_blocks(slack_token, slack_channel, [
         {
@@ -46,14 +52,13 @@ for github_project in github_company_repositories:
             }
         }
     ])
-    break
-    if (stats.total_pr_reviewed + stats.total_pr_created) == 0:
-        continue
+    by_developer = group_by_developer(pull_requests_merged + pull_requests_open)
     # sort reviewer by contribution
-    reviewers = sorted(stats.data.keys(),
-                       key=lambda x: stats.data[x].pr_created_contribution + stats.data[x].pr_review_contribution,
+    reviewers = sorted(by_developer.keys(),
+                       key=lambda x: by_developer[x].contribution + by_developer[x].review_contribution,
                        reverse=True)
-    if len(reviewers) > 0:
+
+    if len(by_developer) > 0:
         send_slack_message_blocks(slack_token, slack_channel, [
             {
                 "type": "section",
@@ -63,24 +68,25 @@ for github_project in github_company_repositories:
                 }
             }
         ], thread.data['ts'])
-    for developer in reviewers:
-        value = stats.data[developer]
-        header = f'`{developer}`\n'
-        msg = ''
-        msg += f'PR created: {value.pr_created_count}\n'
-        msg += f'PR created contribution: {value.pr_created_contribution}\n'
-        msg += f'PR reviewed contribution: {value.pr_review_contribution}\n'
-        pr_reviewed_links = " - ".join(
-            map(lambda pn: f'<https://github.com/{GITHUB_COMPANY_NAME}/{github_project}/pull/{pn}|#{pn}>',
-                value.pr_reviewed))
-        msg += f'PR reviewed: {len(value.pr_reviewed)} {pr_reviewed_links}\n'
 
-        send_slack_message_blocks(slack_token, slack_channel, [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f'{header}```{msg}```'
+        for developer_contribution in by_developer.values():
+            developer = developer_contribution.developer
+            header = f'`{developer.name if developer.name else "n/a"} (@{developer.login_name})`\n'
+            msg = ''
+            msg += f'PR created: {len(developer_contribution.pr_created)}\n'
+            msg += f'PR created contribution: {developer_contribution.contribution}\n'
+            msg += f'PR reviewed contribution: {developer_contribution.review_contribution}\n'
+            pr_reviewed_links = " - ".join(
+                map(lambda pn: f'<https://github.com/{GITHUB_COMPANY_NAME}/{repository}/pull/{pn}|#{pn}>',
+                    developer_contribution.pr_reviewed))
+            msg += f'PR reviewed: {len(developer_contribution.pr_reviewed)} {pr_reviewed_links}\n'
+
+            send_slack_message_blocks(slack_token, slack_channel, [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'{header}```{msg}```'
+                    }
                 }
-            }
-        ], thread.data['ts'])
+            ], thread.data['ts'])
--- src/utils/github2.py
+++ src/utils/github2.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
-from typing import Literal, List
+from typing import Literal, List, Union, TypedDict
 from datetime import datetime
 from github import Github
 from github.PullRequest import PullRequest as GithubPullRequest
 
 from utils.env import GITHUB_TOKEN, GITHUB_COMPANY_NAME
 
-PRStatus = Literal["open", "closed", "all"]
+PRStatus = Literal["open", "closed", "draft"]
+
 
 @dataclass(frozen=True)
 class GithubUser:
@@ -21,44 +22,85 @@ class PullRequest:
 
     def __init__(self, pr: GithubPullRequest):
         self.state: PRStatus = pr.state
+        self.number: int = pr.number
         self.title: str = pr.title
-        self.author_login: GithubUser = GithubUser(pr.user.login, pr.user.name)
+        self.author: GithubUser = GithubUser(pr.user.login, pr.user.name)
         self.merged: bool = pr.merged
         self.merged_by: GithubUser = GithubUser(pr.merged_by.login, pr.merged_by.name) if pr.merged else None
         self.additions: int = pr.additions
         self.deletions: int = pr.deletions
         self.created_at: datetime = pr.created_at
         self.url: str = pr.html_url
+        self.contribution = self.additions + self.deletions
         self.reviewers = set(map(lambda r: GithubUser(r.user.login, r.user.name),
                                  filter(lambda r: r.state in ("APPROVED", "CHANGES_REQUESTED"), pr.get_reviews())))
 
 
-def get_repo_stats2(repository_name: str, days_before: int, state: PRStatus = "all") -> List[PullRequest]:
+def get_repo_stats2(repository_name: str, days_before: int, state: Union[PRStatus, Literal["all"]] = "all") -> List[
+    PullRequest]:
     now = datetime.now()
     g = Github(GITHUB_TOKEN)
     repo = g.get_repo(f"{GITHUB_COMPANY_NAME}/{repository_name}")
     data = repo.get_pulls(state=state, sort="created", direction="desc")
     pull_requests = []
     for pull in data:
-        # the PR is older than we are looking for
+        # the PR is older than we are looking for, break since the following ones are older
         if (now - pull.updated_at).days > days_before:
             break
         pull_requests.append(PullRequest(pull))
     return pull_requests
 
 
-def group_by_state(pull_requests: List[PullRequest]):
-    by_state = {"created": len(pull_requests)}
+class PRStatusCounter(TypedDict):
+    merged: List[PullRequest]
+    open: List[PullRequest]
+    closed: List[PullRequest]
+    reviewed: List[PullRequest]
+    draft: List[PullRequest]
+
+
+def group_by_state(pull_requests: List[PullRequest]) -> PRStatusCounter:
+    """
+    return a dictionary where the key is the PR state e the value the amount of the PR in that state
+    states can be: open, closed, draft, merged, reviewed
+    a PR can be simultaneously in merged and reviewed state (it means it has been merged and someone approved it)
+    :param pull_requests:
+    :return:
+    """
+    by_state = {}
     for pr in pull_requests:
         if pr.merged:
-            by_state["merged"] = by_state.get("merged", 0) + 1
+            by_state["merged"] = by_state.get("merged", [])
+            by_state["merged"].append(pr)
             if len(pr.reviewers):
-                by_state["reviewed"] = by_state.get("reviewed", 0) + 1
-        elif pr.state == "open":
-            by_state["open"] = by_state.get("open", 0) + 1
-        elif pr.state == "draft":
-            by_state["draft"] = by_state.get("draft", 0) + 1
-        elif pr.state == "closed":
-            print(pr.url)
-            by_state["closed"] = by_state.get("closed", 0) + 1
+                by_state["reviewed"] = by_state.get("reviewed", [])
+                by_state["reviewed"].append(pr)
+            continue
+        by_state[pr.state] = by_state.get(pr.state, [])
+        by_state[pr.state].append(pr)
     return by_state
+
+
+@dataclass
+class DeveloperContribution:
+    developer: GithubUser
+    contribution: int
+    review_contribution: int
+    pr_reviewed: List[str]
+    pr_created: List[str]
+
+
+
+def group_by_developer(pull_requests: List[PullRequest]):
+    by_developer = {}
+    for pr in pull_requests:
+        developer_contribution = by_developer.get(pr.author.login_name, DeveloperContribution(pr.author, 0, 0, [], []))
+        developer_contribution.contribution += pr.contribution
+        developer_contribution.pr_created.append(pr.number)
+        by_developer[developer_contribution.developer.login_name] = developer_contribution
+        for review in pr.reviewers:
+            review_developer_contribution = by_developer.get(review.login_name, DeveloperContribution(review, 0, 0, [], []))
+            review_developer_contribution.review_contribution += pr.contribution
+            review_developer_contribution.pr_reviewed.append(pr.number)
+            by_developer[review_developer_contribution.developer.login_name] = review_developer_contribution
+    return by_developer
