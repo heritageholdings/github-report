1f800bd0d27ed45f8b93678c7baf5c98aa7066c6
Matteo Boschi <mbmaken@gmail.com>
2023-01-26 11:02:48 +0100

    disabled until the pair programming session start

--- src/reminder.py
+++ src/reminder.py
@@ -16,5 +16,7 @@ def send_reminder(msg: str):
 	])
 
 
+'''
 send_reminder(
 	":fire: :computer: Today from `15.30` to `16.30` we have the *<https://www.notion.so/heritageholdings/Pair-Programming-e60f0e144d6348d4a71d846be2963e69|Pair Programming>* sessions. Don't miss it, see you there!")
+'''
