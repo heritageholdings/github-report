6a024454eb11aae4655a678aad0019c7291593e3
Matteo Boschi <mbmaken@gmail.com>
2023-02-15 14:29:27 +0100

    add pairs schedule

--- src/reminder.py
+++ src/reminder.py
@@ -1,4 +1,6 @@
 import os
+
+from utils.pair_programming import get_today_programming_pairs
 from utils.slack import send_slack_message_blocks
 
 SLACK_CHANNEL = os.getenv('SLACK_CHANNEL', 'test_feed')
@@ -16,5 +18,11 @@ def send_reminder(msg: str):
 	])
 
 
+pairs = get_today_programming_pairs()
+msg = ''
+if pairs:
+	msg += f'\n\n:busts_in_silhouette: today schedule\n'
+	for pair in pairs:
+		msg += f'- {pair[0]} and {pair[1]}\n'
 send_reminder(
-	":fire: :computer: Today from `15.30` to `16.30` we have the *<https://www.notion.so/heritageholdings/Pair-Programming-e60f0e144d6348d4a71d846be2963e69|Pair Programming>* sessions. Don't miss it, see you there!")
+	f":fire: :computer: Today from `15.30` to `16.30` we have the *<https://www.notion.so/heritageholdings/Pair-Programming-e60f0e144d6348d4a71d846be2963e69|Pair Programming>* sessions. Don't miss it, see you there!{msg}")
---
+++ src/utils/pair_programming.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+scheduling = ((("Matteo", "Simone"), ("Diego", "Fabrizio"), ("Federico", "Rocco")),  # 6 -> 21 -> 0
+			  (("Federico", "Diego"), ("Rocco", "Matteo"), ("Simone", "Fabrizio")),  # 9 -> +3 -> 3
+			  (("Diego", "Simone"), ("Matteo", "Federico"), ("Rocco", "Fabrizio")),  # 13 -> +7 -> 7
+			  (("Rocco", "Diego"), ("Fabrizio", "Matteo"), ("Simone", "Federico")),  # 16 -> +11 -> 11
+			  (("Diego", "Matteo"), ("Federico", "Fabrizio"), ("Rocco", "Simone")))  # 20 -> +15 -> 15
+
+starting_date = datetime(2023, 2, 6)
+round_indexes = (0, 3, 7, 10, 14)
+scheduling_days = (0, 3)  # monday and thursday
+
+
+def get_today_programming_pairs():
+	'''
+	:return: a tuple of tuple containing the pairs of the day. if the day is not a pairing day, it returns None
+	'''
+	today = datetime.today()
+	days_diff_origin = (today - starting_date).days
+	week_diff = int(days_diff_origin / 7)
+	days_diff = days_diff_origin - week_diff * 7
+	# we jump the session when a round has been completed
+	is_jumping_day = days_diff_origin - week_diff * 7 == 3 and week_diff % 3 == 2
+	is_pairing_day = (not is_jumping_day) and days_diff in scheduling_days
+	if is_pairing_day:
+		return scheduling[round_indexes.index(days_diff_origin % 21)]
