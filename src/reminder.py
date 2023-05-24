884509c82983e453cdc97efd101919b5249b804b
Matteo Boschi <mbmaken@gmail.com>
2023-05-24 10:26:06 +0200

    add emoji

--- src/reminder.py
+++ src/reminder.py
@@ -1,6 +1,6 @@
 import os
 
-from utils.pair_programming import get_today_programming_pairs
+from utils.pair_programming import get_today_programming_pairs, dev_emoji_map
 from utils.slack import send_slack_message_blocks
 
 SLACK_CHANNEL = os.getenv('SLACK_CHANNEL', 'test_feed')
@@ -19,10 +19,11 @@ def send_reminder(msg: str):
 
 
 pairs = get_today_programming_pairs()
-msg = ''
+msg = '\n\n'
 if pairs:
-	msg += f'\n\n:busts_in_silhouette:\n'
 	for pair in pairs:
-		msg += f'- {pair[0]} and {pair[1]}\n'
+		one = dev_emoji_map.get(pair[0], pair[0])
+		two = dev_emoji_map.get(pair[1], pair[1])
+		msg += f'{one} {two}\n'
 	send_reminder(
-		f":fire: :computer: Today from `15.30` to `16.30` we have the *<https://www.notion.so/heritageholdings/Pair-Programming-e60f0e144d6348d4a71d846be2963e69|Pair Programming>* sessions. Don't miss it, see you there!{msg}")
+		f":fire: :computer: Today from `15.30` to `16.30` we have the *<https://www.notion.so/heritageholdings/Pair-Programming-e60f0e144d6348d4a71d846be2963e69|Pair Programming>* sessions. \nDon't miss it, see you there!{msg}")
--- src/utils/pair_programming.py
+++ src/utils/pair_programming.py
@@ -10,12 +10,19 @@ starting_date = datetime(2023, 2, 6)
 round_indexes = (0, 3, 7, 10, 14)
 scheduling_days = (0, 3)  # monday and thursday
 
+dev_emoji_map = {"Matteo": ":boschi:",
+				 "Simone": ":biffi:",
+				 "Diego": ":pasquali:",
+				 "Rocco": ":lucia:",
+				 "Federico": ":feroldi:",
+				 "Fabrizio": ":filizola:"}
+
 
 def get_today_programming_pairs():
 	'''
 	:return: a tuple of tuple containing the pairs of the day. if the day is not a pairing day, it returns None
 	'''
-	today = datetime.today()
+	today = datetime(2023, 5, 25)#datetime.today()
 	days_diff_origin = (today - starting_date).days
 	week_diff = int(days_diff_origin / 7)
 	days_diff = days_diff_origin - week_diff * 7
