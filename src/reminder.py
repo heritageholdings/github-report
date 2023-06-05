944826bb2b34cefaeb99e100a836a35ef548ed38
Matteo Boschi <mbmaken@gmail.com>
2023-06-05 12:43:41 +0200

    Update reminder.py

--- src/reminder.py
+++ src/reminder.py
@@ -23,6 +23,6 @@ msg = '\n\n'
 if pairs:
 	for pair in pairs:
 		one, two = pair[0], pair[1]
-		msg += f'- {one} {two}\n'
+		msg += f'- {one} - {two}\n'
 	send_reminder(
 		f":fire: :computer: Today from `15.30` to `16.30` we have the *<https://www.notion.so/heritageholdings/Pair-Programming-e60f0e144d6348d4a71d846be2963e69|Pair Programming>* sessions. \nDon't miss it, see you there!{msg}")
